7a503991e218118f18b78a40fd8807992596affe
rgendron <reid_gendron@sfu.ca>
2021-04-05 17:41:27 -0700

    login-logout feature implemented

--- __init__.py
+++ __init__.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template, url_for, flash, redirect
+from flask import Flask, request, render_template, url_for, redirect, session
 import sqlite3
 
 conn = sqlite3.connect('database.db')
@@ -10,46 +10,79 @@ def dict_factory(cursor, row):
     for idx, col in enumerate(cursor.description):
         d[col[0]] = row[idx]
     return d
-
+    
 @app.route("/")
-@app.route("/login")
+@app.route("/login", methods=['POST','GET'])
 def login():
-    return render_template('log-in.html')
+    if request.method == "POST":
+        # Grab "users" and "passwords" from db
+        conn = sqlite3.connect('database.db')
+        conn.row_factory = dict_factory
+        c = conn.cursor()
+        c.execute("SELECT landlordID, firstName FROM Landlord")
+        user_storage = c.fetchall()
+        
+        # Store user-password combinations in python dictionary
+        users = {}
+        for user in user_storage:
+            users[user['firstName']] = str(user['landlordID'])
+        print(users) # for debugging
     
-user_storage = {'user':'pass','user2':'pass2'}
-    
-@app.route("/home", methods=['POST','GET'])
-def home():
-    username = request.form['username']
-    pwd = request.form['pwd']
-    
-    if username not in user_storage:
-        return render_template('log-in.html', info='invalid user')
-    else:
-        if user_storage[username] != pwd:
-            return render_template('log-in.html', info='invalid password')
+        # Grab form data from login page
+        username = request.form['username']
+        pwd = request.form['pwd']
+        
+        # Check form data for a user-password combination match
+        if username not in users:
+            return render_template('log-in.html', info='invalid user')
         else:
-            return render_template('home.html', user=username)
+            if users[username] != pwd:
+                return render_template('log-in.html', info='invalid password')
+            else:
+                session['user'] = username
+                return render_template('home.html', user=username)
+    else:
+        return render_template('log-in.html')
     
+@app.route("/logout")
+def logout():
+    session.pop('user', None)
+    return redirect(url_for('login'))
+    
+@app.route("/home")
+def home():
+    if 'user' in session:
+        username = session['user']
+        return render_template('home.html', user=username)
+    else:
+        return redirect(url_for('login'))
+            
 @app.route("/landlords")
 def landlords():
-    conn = sqlite3.connect('database.db')
-    conn.row_factory = dict_factory
-    c = conn.cursor()
-    c.execute("SELECT * FROM Landlord")
-    landlords = c.fetchall()
-
-    return render_template('landlords.html', data=landlords)
+    if 'user' in session:
+        username = session['user']
+        conn = sqlite3.connect('database.db')
+        conn.row_factory = dict_factory
+        c = conn.cursor()
+        c.execute("SELECT * FROM Landlord")
+        landlords = c.fetchall()
+        return render_template('landlords.html', data=landlords)
+    else:
+        return redirect(url_for('login'))
     
 @app.route("/properties")
 def properties():
-    conn = sqlite3.connect('database.db')
-    conn.row_factory = dict_factory
-    c = conn.cursor()
-    c.execute("SELECT * FROM Property")
-    properties = c.fetchall()
+    if 'user' in session:
+        username = session['user']
+        conn = sqlite3.connect('database.db')
+        conn.row_factory = dict_factory
+        c = conn.cursor()
+        c.execute("SELECT * FROM Property")
+        properties = c.fetchall()
+        return render_template('properties.html', data=properties)
+    else:
+        return redirect(url_for('login'))
 
-    return render_template('properties.html', data=properties)
 
 
 # @app.route("/register", methods=['GET', 'POST'])
