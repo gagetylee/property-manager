d15eda0902d7a0b43808f4ff44486b875b6e8039
yingc <wanyingc@sfu.ca>
2021-04-14 03:12:30 -0700

    Updated database values for Retail table and Spaces table.

--- testdatabase.sql
+++ testdatabase.sql
@@ -114,78 +114,68 @@ INSERT INTO Landlord (firstName, lastName) VALUES ('Thomas','Clark');
 
 
 -- INSERT PROPERTIES --
-INSERT INTO Property (province,street,postcode,price,monthlyIncome,lotSize,buildDate,landlordID) 
+INSERT INTO Property (province,street,postcode,price,monthlyIncome,lotSize,buildDate,landlordID)
 VALUES ('British Columbia','850  Haaglund Rd','V0H1M0','1234.56','5500','1000','2000','1');
 
-INSERT INTO Property (province,street,postcode,price,monthlyIncome,lotSize,buildDate,landlordID) 
+INSERT INTO Property (province,street,postcode,price,monthlyIncome,lotSize,buildDate,landlordID)
 VALUES ('Saskatchewan','1187  Lillooet Street','S6V1B3','1000000.00','10000','3400','2021','1');
 
-INSERT INTO Property (province,street,postcode,price,monthlyIncome,lotSize,buildDate,landlordID) 
+INSERT INTO Property (province,street,postcode,price,monthlyIncome,lotSize,buildDate,landlordID)
 VALUES ('Ontario','1649  Sheppard Ave','M1S1T4','15003.45','7234','5500','2021','2');
 
-INSERT INTO Property (province,street,postcode,price,monthlyIncome,lotSize,buildDate,landlordID) 
+INSERT INTO Property (province,street,postcode,price,monthlyIncome,lotSize,buildDate,landlordID)
 VALUES ('Quebec','3995  rue Boulay','J0H1Y0','3500.00','3000','1500','1987','3');
 
-INSERT INTO Property (province,street,postcode,price,monthlyIncome,lotSize,buildDate,landlordID) 
+INSERT INTO Property (province,street,postcode,price,monthlyIncome,lotSize,buildDate,landlordID)
 VALUES ('Ontario','2934  Balmy Beach Road','N4K2N7','12330.04','3599','3500','2001','2');
 
- INSERT INTO Property (province,street,postcode,price,monthlyIncome,lotSize,buildDate,landlordID) 
+ INSERT INTO Property (province,street,postcode,price,monthlyIncome,lotSize,buildDate,landlordID)
 VALUES ('Vancouver','106 588 45th Ave','V5C4G2','20345.72','5599','6000','2019','1');
 
-INSERT INTO Property (province,street,postcode,price,monthlyIncome,lotSize,buildDate,landlordID) 
+INSERT INTO Property (province,street,postcode,price,monthlyIncome,lotSize,buildDate,landlordID)
 VALUES ('Vancouver','563 Union St','V6A2B7','13330.22','3599','3500','1999','6');
 
-INSERT INTO Property (province,street,postcode,price,monthlyIncome,lotSize,buildDate,landlordID) 
+INSERT INTO Property (province,street,postcode,price,monthlyIncome,lotSize,buildDate,landlordID)
 VALUES ('Vancouver','3008 8th Ave','V6A2A6','19999.99','5900','6000','2017','1');
 
-INSERT INTO Property (province,street,postcode,price,monthlyIncome,lotSize,buildDate,landlordID) 
+INSERT INTO Property (province,street,postcode,price,monthlyIncome,lotSize,buildDate,landlordID)
 VALUES ('Vancouver','1410 Tolmie St','V6R4B3','10050.00','5000','4599','2018','2');
 
-INSERT INTO Property (province,street,postcode,price,monthlyIncome,lotSize,buildDate,landlordID) 
-VALUES ('Vancouver','59080 Battison St','V5R4M8','12330.04','5000','5000','2001','100002');
+INSERT INTO Property (province,street,postcode,price,monthlyIncome,lotSize,buildDate,landlordID)
+VALUES ('Vancouver','59080 Battison St','V5R4M8','12330.04','5000','5000','2001','4');
 
-INSERT INTO Property (province,street,postcode,price,monthlyIncome,lotSize,buildDate,landlordID) 
-VALUES ('Vancouver','3308 Ash St','V5C3E3','10099.99','7999','5500','2001','100004');
+INSERT INTO Property (province,street,postcode,price,monthlyIncome,lotSize,buildDate,landlordID)
+VALUES ('Vancouver','3308 Ash St','V5C3E3','10099.99','7999','5500','2001','4');
 
-INSERT INTO Property (province,street,postcode,price,monthlyIncome,lotSize,buildDate,landlordID) 
-VALUES ('Ontario','1847 Ross Street','G4T7W2','903332.12','3420','2591','1989','100004');
+INSERT INTO Property (province,street,postcode,price,monthlyIncome,lotSize,buildDate,landlordID)
+VALUES ('Ontario','1847 Ross Street','G4T7W2','903332.12','3420','2591','1989','5');
 
-INSERT INTO Property (province,street,postcode,price,monthlyIncome,lotSize,buildDate,landlordID) 
-VALUES ('Saskatchewan','2431 Main St','V4K5K7','23212.14','3192','3310','2011','100004');
+INSERT INTO Property (province,street,postcode,price,monthlyIncome,lotSize,buildDate,landlordID)
+VALUES ('Saskatchewan','2431 Main St','V4K5K7','23212.14','3192','3310','2011','6');
 
-INSERT INTO Property (province,street,postcode,price,monthlyIncome,lotSize,buildDate,landlordID) 
-VALUES ('Alberta','9096 Algoma St Road','P7A4T3','12330.38','2345','2510','1999','100004');
+INSERT INTO Property (province,street,postcode,price,monthlyIncome,lotSize,buildDate,landlordID)
+VALUES ('Alberta','9096 Algoma St Road','P7A4T3','12330.38','2345','2510','1999','5');
 
-INSERT INTO Property (province,street,postcode,price,monthlyIncome,lotSize,buildDate,landlordID) 
-VALUES ('Quebec','4610  rue Saint-Antoine','J4K8L7','32905.33','3500','3502','1974','100004');
-
-
+INSERT INTO Property (province,street,postcode,price,monthlyIncome,lotSize,buildDate,landlordID)
+VALUES ('Quebec','4610  rue Saint-Antoine','J4K8L7','32905.33','3500','3502','1974','6');
 
 
 
 
 
-
-
-
-
-
-
-
-
-INSERT INTO MonthlyExpenses (propertyID, date, landID, utilityBill, propertyTax, MaintNRepairs) 
+INSERT INTO MonthlyExpenses (propertyID, date, landID, utilityBill, propertyTax, MaintNRepairs)
 VALUES('1', "2020-08-03", '1','100.32', '303.32', '76.38');
 
-INSERT INTO MonthlyExpenses (propertyID, date, landID, utilityBill, propertyTax, MaintNRepairs) 
+INSERT INTO MonthlyExpenses (propertyID, date, landID, utilityBill, propertyTax, MaintNRepairs)
 VALUES('2', "2020-09-03", '1','187.72', '203.87', '33.44');
 
-INSERT INTO MonthlyExpenses (propertyID, date, landID, utilityBill, propertyTax, MaintNRepairs) 
+INSERT INTO MonthlyExpenses (propertyID, date, landID, utilityBill, propertyTax, MaintNRepairs)
 VALUES('3', "2020-10-03", '2','243.62', '340.56', '25.11');
 
-INSERT INTO MonthlyExpenses (propertyID, date, landID, utilityBill, propertyTax, MaintNRepairs) 
+INSERT INTO MonthlyExpenses (propertyID, date, landID, utilityBill, propertyTax, MaintNRepairs)
 VALUES('4', "2020-11-03", '3','43.45', '813.42', '65.76');
 
-INSERT INTO MonthlyExpenses (propertyID, date, landID, utilityBill, propertyTax, MaintNRepairs) 
+INSERT INTO MonthlyExpenses (propertyID, date, landID, utilityBill, propertyTax, MaintNRepairs)
 VALUES('5', "2020-12-03", '2','32.11', '399.74', '25.26');
 
 
@@ -225,17 +215,16 @@ INSERT INTO Rents (propertyID, tenantID)
 VALUES('2', '5');
 INSERT INTO Rents (propertyID, tenantID)
 VALUES('2', '6');
-
 INSERT INTO Rents (propertyID, tenantID)
 VALUES('3', '7');
 
 
 
-INSERT INTO Retail (propertyID) VALUES ('200000');
-INSERT INTO Retail (propertyID) VALUES ('200001');
-INSERT INTO Retail (propertyID) VALUES ('200002');
-INSERT INTO Retail (propertyID) VALUES ('200003');
-INSERT INTO Retail (propertyID) VALUES ('200004');
+INSERT INTO Retail (propertyID) VALUES ('11');
+INSERT INTO Retail (propertyID) VALUES ('12');
+INSERT INTO Retail (propertyID) VALUES ('13');
+INSERT INTO Retail (propertyID) VALUES ('14');
+INSERT INTO Retail (propertyID) VALUES ('15');
 
 
 
@@ -259,11 +248,11 @@ INSERT INTO Multifamily (propertyID,spaces, furnished) VALUES ('9', '8', "No");
 INSERT INTO Multifamily (propertyID,spaces, furnished) VALUES ('10', '6', "Yes");
 
 
-INSERT INTO Spaces (propertyID, space) VALUES ('11', '1');
-INSERT INTO Spaces (propertyID, space) VALUES ('12', '1');
-INSERT INTO Spaces (propertyID, space) VALUES ('13', '3');
-INSERT INTO Spaces (propertyID, space) VALUES ('14', '5');
-INSERT INTO Spaces (propertyID, space) VALUES ('15', '8');
+INSERT INTO Spaces (propertyID, space) VALUES ('6', '1');
+INSERT INTO Spaces (propertyID, space) VALUES ('7', '1');
+INSERT INTO Spaces (propertyID, space) VALUES ('8', '3');
+INSERT INTO Spaces (propertyID, space) VALUES ('9', '5');
+INSERT INTO Spaces (propertyID, space) VALUES ('10', '8');
 
 CREATE VIEW TenantTemp AS
 SELECT tenantID, name, monthlyRent
@@ -276,9 +265,3 @@ FROM (
 	   FROM Individual
    )
 )
-
-
-
-
-
-
